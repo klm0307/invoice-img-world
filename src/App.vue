7f0b76d340059e494cbcc674671e1b4ff75529fa
klm0307 <33470287+klm0307@users.noreply.github.com>
2024-10-30 11:30:44 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="container mx-auto px-4">
+      <nav class="flex flex-wrap">
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
